12443829cceae3e52b3cb704ac65257aabc36191
Artiom Oriol <cim607@gmail.com>
2020-02-23 17:11:18 +0200

    added one more table for credit details

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,39 @@
-// сумма кредита -  80 000 гр 
-// срок - 2 года.
-// процент - 24 %
-// Комиссия – 1 % от суммы.
-// П = 80 000 * 24 / 100 * 730 / 365 + 80 000 * 0, 01 * 24 = 57600 грн.
-// То есть в итоге Сергею придется выплатить 137 600 гривен.
-const moneyAmount = document.getElementById('moneyAmount').value;
-const monthAmount = document.getElementById('month');
-const clientType = document.getElementById('typeOfCredit').value;
-const firstPayMonth = document.getElementById('monthCredit').value;
-const firstPayYear = document.getElementById('yearCredit').value;
+const moneyAmount = document.getElementById('moneyAmount');
+let monthAmount = document.getElementById('month');
+let clientType = document.getElementById('typeOfCredit');
+let firstPayMonth = document.getElementById('monthCredit');
+let firstPayYear = document.getElementById('yearCredit');
 const showCheckBox = document.getElementById('showMonthPayments');
+const commision = 0.035;
+let creditAmount;
+let creditDetails = document.getElementById('creditDetails');
 
-monthValue = (function getMonthValue(x){
-    var x = +monthAmount.value;
-    console.log(x)
-    return x;
-})();
+function calculateTheCredit(){
+    creditAmount = parseInt(moneyAmount.value) * (clientType.value / 10 + (clientType.value / 10)/((Math.pow((1+(clientType.value / 10)), parseInt(monthAmount.value)) - 1))) + (parseInt(moneyAmount.value) * commision);
+    
+    console.log('total Amount money ', creditAmount);
+    console.log(moneyAmount.value);
+    
+    console.log(clientType.value);
+    console.log(firstPayMonth.value);
+    console.log(firstPayYear.value);
+    console.log(commision * moneyAmount.value);
+  
+    let monthlyPayments = document.getElementById('monthlyPayments');
+    let totalCreditCost = document.getElementById('totalCreditCost');
+    let lastMonth = document.getElementById('lastMonth');
 
+    let monthPayments = (creditAmount / monthAmount.value).toFixed(2);
+    let totalCreditCostValue = (creditAmount - moneyAmount.value).toFixed(2);
 
+    monthlyPayments.innerText = monthPayments;
+    totalCreditCost.innerText = totalCreditCostValue; 
 
+    console.log(lastMonth);
+    creditDetails.style = 'display: block;'
+
+}
+
+// сделать табличку где заполняются выплаты по кредиту
+// так же с помощью цикла сделать выведение таблицы по чекбоксу
 
